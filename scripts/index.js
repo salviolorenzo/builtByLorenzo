49170071ae4591ecd922cd50a12f8538a3878ace
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-11-17 18:19:53 -0500

    adds js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-const form
+const form = document.querySelector('[data-form]');
+
+
+window.onload = function () {
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    this.contact_number.value = Math.random() * 100000;
+    emailjs.sendForm('contact_service', 'contact_template', this);
+  });
+}
+
